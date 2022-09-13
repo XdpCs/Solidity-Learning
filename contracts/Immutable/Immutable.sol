5bba8235313c77be5dc0b35b80c6d036975b749e
XdpCs <451679250@qq.com>
2022-09-13 14:40:57 +0800

    docs(immutable.sol): doc changes
    doc changes

--- contracts/Immutable/Immutable.sol
+++ contracts/Immutable/Immutable.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.0;
 // 不可变变量就像常量
 // 不可变变量的值可以在构造函数中初始化或在声明时初始化，但初始化后就不能被修改了
 // 这样可以节省gas,提高合约的安全性
+// 数值变量可以声明为immutable
+// string和bytes不可以声明为immutable
+// 这不同于constant
 contract Immutable {
     // 大写不可变变量的名称是一种编码习惯
     address public immutable MY_ADDRESS;
