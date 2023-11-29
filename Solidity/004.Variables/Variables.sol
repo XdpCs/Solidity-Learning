fe33266468cb1ce9aa9a7a0a8461a5751c7ce1fc
Alan Xu <xdpcsyy@gmail.com>
2023-11-29 15:35:04 +0800

    refactor(04): refactor 04 (#68)
    refactor 04

--- Solidity/004.Variables/Variables.sol
+++ Solidity/004.Variables/Variables.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 contract Variables {
     string public text = "hello,fyy";
@@ -14,5 +14,33 @@ contract Variables {
         gasLeft = gasleft();
         origin = tx.origin;
         timestamp = block.timestamp;
+    }
+
+    function blockHash() view public returns (bytes32) {
+        return blockhash(block.number - 1);
+    }
+
+    function getBaseFee() view public returns (uint) {
+        return block.basefee;
+    }
+
+    function getChainID() view public returns (uint) {
+        return block.chainid;
+    }
+
+    function getCoinBase() view public returns (address) {
+        return block.coinbase;
+    }
+
+    function getDifficulty() view public returns (uint) {
+        return block.difficulty;
+    }
+
+    function getPrevRandao() view public returns (uint) {
+        return block.prevrandao;
+    }
+
+    function getGasLimit() view public returns (uint) {
+        return block.gaslimit;
     }
 }
